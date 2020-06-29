987c9326d50b56288f914fd50e4119dcc5780290
Oscar Medrano Castillo <omedranoc@hotmail.com>
2020-06-29 18:39:41 -0500

    I added somethings about class components , lifecycle and state

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const SeasonDisplay = () =>{
+    return <div>How are you</div>
+}
+
+export default SeasonDisplay
--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonDisplay from './SeasonDisplay'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//function component
+/*const App = () =>{
+    
+    navigator.geolocation.getCurrentPosition( (position) =>{
+        console.log(position)}, (err) => {
+            console.log(err)
+        }
+    )
+    return (
+    <div>
+        Oscar Medrano
+       <SeasonDisplay/>
+    </div>)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+}*/
+
+//class component
+
+class App extends React.Component {
+    
+    constructor(props){
+        super(props)
+        // initialize the state
+        this.state = {lat : null, error : ''}
+        
+        //get current position; the firts argument is an object that contain all the information of the current position
+        navigator.geolocation.getCurrentPosition( 
+            (position) =>{
+                //update the state
+                this.setState({lat : position.coords.latitude})},
+            (err) => {
+                this.setState({error : err.message })
+            }
+        )      
+        
+    }
+
+    render(){        
+       if(this.state.lat && !this.state.error){
+           return <div>Latitud : {this.state.lat}</div>
+       }
+       if(!this.state.lat && this.state.error){
+           return <div>Error : {this.state.error}</div>
+       }
+       return <div>Loanding</div>
+    }
+}
+
+
+
+
+ReactDOM.render( <App/>, document.getElementById('root')  )
